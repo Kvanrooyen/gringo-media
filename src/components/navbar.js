b90077309bdabfe0e91033bfaf69f93f943dd8e7
Keagan <github@governorkeagan.com>
2023-10-08 14:38:27 +0100

    Changed About to About Us

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
     return (
         <nav className="absolute top-0 left-0 right-0 bg-transparent p-4 flex justify-center items-center z-50">
             <Link to="/" className="text-white text-xl font-semibold mx-4">HOME</Link>
-            <Link to="/about" className="text-white text-xl font-semibold mx-4">ABOUT</Link>
+            <Link to="/about" className="text-white text-xl font-semibold mx-4">ABOUT US</Link>
         </nav>
     );
 }
